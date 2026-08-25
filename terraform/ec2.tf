8dc8381542506498b95b945c387db7b98c9bca2b
suraj <surajbele51@gmail.com>
2026-08-25 08:35:01 +0530

    variable block added

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "myec2" {
-  ami = "ami-0332d564d76dbd8d6"
-  instance_type = "t3.micro"
+  ami = "var.myami"
+  instance_type = "var.isntance_type"
   key_name = "id_rsa"
   vpc_security_group_ids = [ "sg-0e3db27617e12fa04" ]
   tags = {
@@ -12,4 +12,12 @@ resource "aws_instance" "myec2" {
     env = "dev"
     Name = "spider"
   }
+}
+
+variable "myami" {
+  default = "ami-0332d564d76dbd8d6"
+  description = "this is us-east-1 ami"
+}
+variable "instance_type" {
+  default = "t3.micro"
 }
