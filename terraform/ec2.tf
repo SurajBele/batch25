bcd3ff22905cb9ea680a3911e46718db0b5f9a14
suraj <surajbele51@gmail.com>
2026-08-25 09:20:07 +0530

    tfvar file added

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,10 +1,9 @@
-
 provider "aws" {
   region = var.region
 }
 resource "aws_instance" "myec2" {
   ami = var.myami
-  instance_type = var.instance_type
+  instance_type = local.instance_type
   key_name = var.key_name
   vpc_security_group_ids = var.mysg
   tags = {
